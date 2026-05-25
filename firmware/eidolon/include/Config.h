e608552978219d27514cdf693a1e28b927dcad66
alaotach <aryanmi2001@gmail.com>
2026-05-25 10:32:38 +0530

    initial commit and config of pins from schema

---
+++ firmware/eidolon/include/Config.h
@@ -0,0 +1,32 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define SSID "Eidolon"
+#define PASSWORD "3.14159265"
+#define CHANNEL 1
+#define MAX_CONNECTIONS 4
+#define SERVER_PORT 80
+
+#define UPLOAD_DIR "/uploads"
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1
+#define OLED_ADDRESS 0x3C
+
+#define I2S_WS 2
+#define I2S_SCK 4
+#define I2S_SD 5
+#define I2S_DIN 6
+
+#define BUTTON_PIN 7
+
+#define MPU_INT 3
+#define MPU_ADDR 0x68
+
+#define OLED_BRIGHTNESS 255
+#define OLED_SCL 9
+#define OLED_SDA 8
+
+
+#endif
